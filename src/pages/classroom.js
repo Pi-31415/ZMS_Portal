05626276ab8dd17f90e4740a4242077de69c4547
Pi-31415 <paingthetko.pi@gmail.com>
2020-07-17 18:02:26 +0630

    classroom

--- src/pages/classroom.js
+++ src/pages/classroom.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { makeStyles } from '@material-ui/core/styles';
+import {Link} from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 export default function Classroom() {
+    const classes = useStyles();
     return (
-        <>
+        <React.Fragment>
+            <CssBaseline />
+            <AppBar>
+                <Toolbar>
+                    <Link to="/portal/dashboard" style={{color:'#fff'}}>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <ArrowBackIcon />
+                    </IconButton>
+                    </Link>
+                    <Typography variant="h6">ZMS Classroom</Typography>
+                </Toolbar>
+            </AppBar>
+            <Toolbar />
             <iframe style={{ width: '100vw', border: 'none', height: '100vh' }} allow="camera *;microphone *" src="https://zipcall.io/join/zms_chem_ctkw" title="W3Schools Free Online Web Tutorials">
             </iframe>
-        </>
-    )
+        </React.Fragment>
+    );
 }
