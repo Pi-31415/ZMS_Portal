f1db9b1adbf543747b0e102fe6ca69281cf0c9d5
Pi-31415 <paingthetko.pi@gmail.com>
2020-07-20 20:16:33 +0630

    anson table

--- src/Anson/syllabustable.js
+++ src/Anson/syllabustable.js
@@ -24,14 +24,10 @@ const useRowStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein, price) {
+function createData(name, calories) {
   return {
     name,
     calories,
-    fat,
-    carbs,
-    protein,
-    price,
     history: [
       { date: '2020-01-05', customerId: '11091700', amount: 3 },
       { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
@@ -56,9 +52,6 @@ function Row(props) {
           {row.name}
         </TableCell>
         <TableCell align="right"><Chip label={row.calories} /></TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -118,11 +111,9 @@ Row.propTypes = {
 };
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+  createData('Frozen yoghurt', 'complete'),
+  createData('Ice cream sandwich', 'not complete'),
+
 ];
 
 export default function SyllabusTable() {
@@ -134,9 +125,6 @@ export default function SyllabusTable() {
             <TableCell />
             <TableCell>Dessert (100g serving)</TableCell>
             <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
