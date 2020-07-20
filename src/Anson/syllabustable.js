d9b3f77c838df73cd32f98021137e21b9bcb8085
Pi-31415 <paingthetko.pi@gmail.com>
2020-07-20 20:43:27 +0630

    syllabus halfway completed

--- src/Anson/syllabustable.js
+++ src/Anson/syllabustable.js
@@ -24,16 +24,102 @@ const useRowStyles = makeStyles({
   },
 });
 
-function createData(name, calories) {
+function createData1(lesson,topic,status) {
   return {
-    name,
-    calories,
-    history: [
-      { date: '2020-01-05', customerId: '11091700', amount: 3 },
-      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
+    lesson,
+    topic,
+    status,
+    content: [
+      { id: '1', description: 'Overview of analytical tools (audience and purpose, context, tone, stylistic devices, structure)'},
+      { id: '1', description: 'How to annotate the text'},
+      {id:'3',description:'Overview of time management for the paper '},
     ],
   };
 }
+
+function createData2(lesson,topic,status) {
+    return {
+      lesson,
+      topic,
+      status,
+      content: [
+        { id: '1', description: 'Outline of the most effective essay structure '},
+        { id: '2', description: 'How to write a thesis'},
+        { id: '3', description: 'How to write an effective introduction and conclusion '},
+        { id: '4', description: 'HOMEWORK: TIMED paper 1 past paper'},
+      ],
+    };
+  }
+  function createData3(lesson,topic,status) {
+    return {
+      lesson,
+      topic,
+      status,
+      content: [
+        { id: '1', description: 'Go over past paper'},
+        { id: '2', description: 'How to write effective body paragraphs; transitions between paragraphs'},
+        { id: '3', description: 'HOMEWORK: TIMED paper 1 past paper '},
+      ],
+    };
+  }
+  function createData4(lesson,topic,status) {
+    return {
+      lesson,
+      topic,
+      status,
+      content: [
+        { id: '1', description: 'Go over past paper'},
+        { id: '2', description: 'Paper 1 wrap-up: key takeaways and important notes'},
+        { id: '3', description: 'Questions'},
+      ],
+    };
+  }
+  function createData5(lesson,topic,status) {
+    return {
+      lesson,
+      topic,
+      status,
+      content: [
+        { id: '1', description: 'Discussion about student’s chosen texts: context, characters'},
+        { id: '2', description: 'How to answer the paper 2 question; how to choose a prompt '},
+      ],
+    };
+  }
+  function createData6(lesson,topic,status) {
+    return {
+      lesson,
+      topic,
+      status,
+      content: [
+        { id: '1', description: 'Continued discussion of chosen texts: themes, quotes'},
+        { id: '2', description: 'Including important quotes, key themes, characters, context, etc.'},
+        { id: '3', description: 'HOMEWORK: short analytical writing assignment to a prompt'},
+      ],
+    };
+  }
+  function createData7(lesson,topic,status) {
+    return {
+      lesson,
+      topic,
+      status,
+      content: [
+        { id: '1', description: ''},
+        { id: '2', description: ''},
+        { id: '3', description: ''},
+        { id: '4', description: ''},
+      ],
+    };
+  }
+  function createData8(lesson,topic,status) {
+    return {
+      lesson,
+      topic,
+      status,
+      content: [
+        { id: '1', description: ''},
+      ],
+    };
+  }
 
 function Row(props) {
   const { row } = props;
@@ -49,37 +135,25 @@ function Row(props) {
           </IconButton>
         </TableCell>
         <TableCell component="th" scope="row">
-          {row.name}
+          {row.lesson}
         </TableCell>
-        <TableCell align="right"><Chip label={row.calories} /></TableCell>
+        <TableCell align="left">{row.topic}</TableCell>
+        <TableCell align="center"><Chip label={row.status} /></TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
+            <Box mb={4} ml={3} mr={3}>
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
+                    <TableCell>Lesson Content</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
+                  {row.content.map((historyRow) => (
+                    <TableRow key={historyRow.id}>
+                      <TableCell>{historyRow.id}{'. '}{historyRow.description}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
@@ -111,8 +185,14 @@ Row.propTypes = {
 };
 
 const rows = [
-  createData('Frozen yoghurt', 'complete'),
-  createData('Ice cream sandwich', 'not complete'),
+  createData1('Lesson 1','What to look for when reading the texts ', 'incomplete'),
+  createData2('Lesson 2','How to structure your essay', 'incomplete'),
+  createData3('Lesson 3','Writing body paragraphs (analysis)', 'incomplete'),
+  createData4('Lesson 4','Wrap up', 'incomplete'),
+  createData5('Lesson 5','Overview and Understanding the texts ', 'incomplete'),
+  createData6('Lesson 6','Planning and brainstorming: crafting a study guide', 'incomplete'),
+  createData7('Lesson 7','Structuring paper 2 ', 'incomplete'),
+  createData8('Lesson 8','Wrap up ', 'incomplete'),
 
 ];
 
@@ -123,8 +203,9 @@ export default function SyllabusTable() {
         <TableHead>
           <TableRow>
             <TableCell />
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
+            <TableCell>Lesson</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell align="center">Status</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
