5242ad10baf51beff9506647c20c25ac38407afd
Pi-31415 <paingthetko.pi@gmail.com>
2020-07-26 17:49:22 +0630

    list item classes disabled

--- src/components/dashboard_list.js
+++ src/components/dashboard_list.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { faCalculator } from '@fortawesome/free-solid-svg-icons';
 import { faUserGraduate } from '@fortawesome/free-solid-svg-icons';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import Avatar from '@material-ui/core/Avatar';
@@ -25,67 +25,71 @@ export default function DashboardList() {
   }, [])
 
   return (
-  <>
-    <List>
-      <div>
+    <>
+      <List>
+        <div>
 
-        <ListItem>
-          <ListItemIcon>
-            <Avatar alt={localStorage.getItem("Username")} src={Avatarimg} />
-          </ListItemIcon>
-          <ListItemText primary={localStorage.getItem("Username")} />
-        </ListItem>
-        <Link to="/portal/dashboard/home" className="custom-link-normal">
-          <ListItem button>
+          <ListItem>
             <ListItemIcon>
-              <FontAwesomeIcon icon={faHome} size='2x' />
+              <Avatar alt={localStorage.getItem("Username")} src={Avatarimg} />
             </ListItemIcon>
-            <ListItemText primary="Home" />
+            <ListItemText primary={localStorage.getItem("Username")} />
           </ListItem>
-        </Link>
-        <Link to="/portal/dashboard/schedule" className="custom-link-normal">
-          <ListItem button>
-            <ListItemIcon>
-              <FontAwesomeIcon icon={faCalendarAlt} size='2x' />
-            </ListItemIcon>
-            <ListItemText primary="Schedule" />
-          </ListItem>
-        </Link>
-      </div>
-</List>
-<Divider></Divider>
-<List>
-      <div>
-        <ListItem>
-          <ListItemIcon>
-            <FontAwesomeIcon icon={faUserGraduate} size='2x' />
-          </ListItemIcon>
-          <ListItemText primary="My Classes" style={{ color: '#9b1c31' }} />
-        </ListItem>
-        <Link to="/portal/syllabus/anson" className="custom-link-normal">
-          <ListItem button>
-            <ListItemIcon>
-              <FontAwesomeIcon icon={faBook} size='2x' />
-            </ListItemIcon>
-            <ListItemText primary="IB English" />
-          </ListItem>
-        </Link>
-      </div>
-</List>
-<Divider></Divider>
-<List>
-      <div>
-        <Link to="/portal/" className="custom-link-normal">
-          <ListItem button>
-            <ListItemIcon>
-              <FontAwesomeIcon icon={faSignOutAlt} size='2x' />
-            </ListItemIcon>
-            <ListItemText primary="Log Out" />
-          </ListItem>
-        </Link>
-      </div>
+          <Link to="/portal/dashboard/home" className="custom-link-normal">
+            <ListItem button>
+              <ListItemIcon>
+                <FontAwesomeIcon icon={faHome} size='2x' />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </Link>
+          <Link to="/portal/dashboard/schedule" className="custom-link-normal">
+            <ListItem button>
+              <ListItemIcon>
+                <FontAwesomeIcon icon={faCalendarAlt} size='2x' />
+              </ListItemIcon>
+              <ListItemText primary="Schedule" />
+            </ListItem>
+          </Link>
+        </div>
       </List>
-</>
-);
-  }
+      <Divider></Divider>
+      <List>
+        <div>
+          <ListItem>
+          <ListItemIcon>
+            
+          </ListItemIcon>
+            <ListItemText primary={<b>My Classes</b>} />
+          </ListItem>
+          {/*
+            <Link to="/portal/syllabus/anson" className="custom-link-normal">
+        */}
+          <ListItem>
+            <ListItemIcon>
+              <FontAwesomeIcon icon={faCalculator} size='2x' />
+            </ListItemIcon>
+            <ListItemText primary="IB Mathematics" />
+          </ListItem>
+          {/*
+            </Link>
+          */}
+        </div>
+      </List>
+      <Divider></Divider>
+      <List>
+        <div>
+          <Link to="/portal/" className="custom-link-normal">
+            <ListItem button>
+              <ListItemIcon>
+                <FontAwesomeIcon icon={faSignOutAlt} size='2x' />
+              </ListItemIcon>
+              <ListItemText primary="Log Out" />
+            </ListItem>
+          </Link>
+        </div>
+      </List>
+    </>
+  );
+}
 
