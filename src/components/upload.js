de96da14bec4a85d9e8b919d24c02d79f4f7796a
Pi-31415 <paingthetko.pi@gmail.com>
2020-07-30 22:29:57 +0630

    file upload test

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,56 +1,64 @@
-import React, { useState } from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import axios from "axios";
+import React from 'react'
+import axios from 'axios';
 
+class FileUpload extends React.Component{
 
-export default function Upload() {
-    const [file, setFile] = React.useState();
-    const [lessonid, setLessonid] = React.useState("");
+    constructor(){
+        super();
+        this.state = {
+            selectedFile:'',
+        }
 
-    const onChange = (e) => {
-        console.log(e.target.files[0]);
-        setFile(e.target.files[0]);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        const query = {
-            "LESSON_ID": "TEST LESSON",
-            "FILE": file
-        };
-        axios.post('https://zmsedu.com/api/student/homework/upload', query)
-            .then(response => console.log(response.data.RESULT))
-            .catch(error => {
-                console.log(error)
-            });
+    handleInputChange(event) {
+        this.setState({
+            selectedFile: event.target.files[0],
+          })
     }
 
-    return (
-        <React.Fragment>
-            <Card>
-                <CardContent align="center">
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="upload-photo">
-                            <input
-                                id="upload-photo"
-                                name="upload-photo"
-                                type="file"
-                                onChange={onChange}
-                            />
+    submit(){
+        const data = new FormData() 
+        data.append('LESSON_ID', 'TEST ID')
+        data.append('FILE', this.state.selectedFile)
+        console.warn(this.state.selectedFile);
+        let url = "https://zmsedu.com/api/student/homework/upload";
 
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                            >
-                                Upload
-                    </Button>
-                        </label>
-                    </form>
-                </CardContent>
-            </Card>
-        </React.Fragment>
-    );
+        axios.post(url, data, { // receive two parameter endpoint url ,form data 
+        })
+        .then(res => { // then print response status
+            console.warn(res);
+        })
+
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <br /><br />
+
+                            <h3 className="text-white">React File Upload - Nicesnippets.com</h3>
+                            <br />
+                            <div className="form-row">
+                                <div className="form-group col-md-6">
+                                    <label className="text-white">Select File :</label>
+                                    <input type="file" className="form-control" name="upload_file" onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+
+                            <div className="form-row">
+                                <div className="col-md-6">
+                                    <button type="submit" className="btn btn-dark" onClick={()=>this.submit()}>Save</button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        )  
+    }
 }
+
+export default FileUpload;
