398bfb4235b4bd9ca768103ecdf625253e0c2a1e
Pi-31415 <paingthetko.pi@gmail.com>
2020-07-30 22:42:00 +0630

    Update upload.js

--- src/components/upload.js
+++ src/components/upload.js
@@ -40,7 +40,7 @@ class FileUpload extends React.Component{
                     <div className="col-md-6 offset-md-3">
                         <br /><br />
 
-                            <h3 className="text-white">React File Upload - Nicesnippets.com</h3>
+                            <h3 className="text-white">File Upload Test</h3>
                             <br />
                             <div className="form-row">
                                 <div className="form-group col-md-6">
