0a76926301d6d071fa183ace5c905d92b779a179
Pi-31415 <paingthetko.pi@gmail.com>
2020-07-30 21:44:36 +0630

    upload script

---
+++ src/components/upload.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+class Upload extends React.Component {
+    render() {
+        return (
+            <Typography variant="body2" color="textSecondary" align="center">
+                {'Copyright © ZMS Education & Technology '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        );
+    }
+}
+export default Upload;
