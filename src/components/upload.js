d3027d5e6fc03ab98c217e2fee499fe2e7323e8f
Pi-31415 <paingthetko.pi@gmail.com>
2020-07-30 22:20:00 +0630

    file

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,31 +1,56 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-//import axios from "axios";
+import axios from "axios";
 
-class Upload extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Card>
-                    <CardContent align="center">
+
+export default function Upload() {
+    const [file, setFile] = React.useState();
+    const [lessonid, setLessonid] = React.useState("");
+
+    const onChange = (e) => {
+        console.log(e.target.files);
+        setFile(e.target.files);
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        const query = {
+            "LESSON_ID": "TEST LESSON",
+            "FILE": file
+         };
+         axios.post('https://zmsedu.com/api/student/homework/upload', query)
+         .then(response => console.log(response.data.RESULT))
+         .catch(error => {
+           console.log(error)
+         });
+    }
+
+    return (
+        <React.Fragment>
+            <Card>
+                <CardContent align="center">
+                    <form onSubmit={handleSubmit}>
                         <label htmlFor="upload-photo">
                             <input
                                 id="upload-photo"
                                 name="upload-photo"
                                 type="file"
+                                onChange={onChange}
                             />
 
-                            <Button color="secondary" variant="contained" component="span">
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                            >
                                 Upload
-                            </Button>
+                    </Button>
                         </label>
-                    </CardContent>
-                </Card>
-            </React.Fragment>
-        );
-    }
+                    </form>
+                </CardContent>
+            </Card>
+        </React.Fragment>
+    );
 }
-export default Upload;
