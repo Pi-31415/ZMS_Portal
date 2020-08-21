c42d454156a3b9742c16e5c73d63485c5b753959
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-21 17:46:31 +0630

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,19 @@ import {
 } from 'react-router-dom';
 
 
-//<Route path="/portal/signup"><SignUp></SignUp></Route>
+/*
+
+<Route path="/portal/signup"><SignUp></SignUp></Route>
+            <Route path="/portal/accountcreated"><AccountCreated></AccountCreated></Route>
+            <Route path="/portal/dashboard/home"><Dashboard></Dashboard></Route>
+            <Route path="/portal/dashboard/schedule"><Schedule></Schedule></Route>
+            <Route path="/portal/dashboard/notif"><Notifications></Notifications></Route>
+            <Route path="/portal/dashboard/syllabus"><Syllabus></Syllabus></Route>
+            <Route path="/portal/terms"><Terms></Terms></Route>
+            <Route path="/portal/privacy"><Privacy></Privacy></Route>
+            <Route path="/portal/classroom"><Classroom></Classroom></Route>
+            <Route path="/portal"><SignInSide></SignInSide></Route>
+*/
 
 const App = props => {
   return (
@@ -16,8 +28,10 @@ const App = props => {
         <div style={{ padding: '0', margin: '0' }}>
           <Switch>
             
+
           </Switch>
         </div>
+
       </div>
     </Router>
   );
