67b7cb14bbffa7c1bd8d3e94418c5116423b0452
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-21 17:45:29 +0630

    .

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ import {
 } from 'react-router-dom';
 
 
-//Add Pages
-import SignInSide from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Dashboard from './pages/dashboard';
-import Terms from './pages/terms';
-import Privacy from './pages/privacy';
-import Classroom from './pages/classroom';
-import Schedule from './pages/schedule';
-import Notifications from './pages/notifications';
-//Anson only
-import Syllabus from './components/syllabus';
-import AccountCreated from './pages/accountcreated';
+//<Route path="/portal/signup"><SignUp></SignUp></Route>
 
 const App = props => {
   return (
@@ -26,43 +15,9 @@ const App = props => {
       <div>
         <div style={{ padding: '0', margin: '0' }}>
           <Switch>
-            <Route path="/portal/signup"><SignUp></SignUp></Route>
-            <Route path="/portal/accountcreated"><AccountCreated></AccountCreated></Route>
-            <Route path="/portal/dashboard/home"><Dashboard></Dashboard></Route>
-            <Route path="/portal/dashboard/schedule"><Schedule></Schedule></Route>
-            <Route path="/portal/dashboard/notif"><Notifications></Notifications></Route>
-            <Route path="/portal/dashboard/syllabus"><Syllabus></Syllabus></Route>
-            <Route path="/portal/terms"><Terms></Terms></Route>
-            <Route path="/portal/privacy"><Privacy></Privacy></Route>
-            <Route path="/portal/classroom"><Classroom></Classroom></Route>
-            <Route path="/portal"><SignInSide></SignInSide></Route>
-
+            
           </Switch>
         </div>
-
-     
-       {/*
-        <div style={{ background: '#009688', padding: '0', margin: '0', color: '#fff' }}>
-          <b>(Development Mode) Completed Pages : </b>
-          <Link to="/portal" style={{ color: '#fff' }}>Log In</Link>
-          {' | '}
-          <Link to="/portal/signup" style={{ color: '#fff' }}>Sign Up</Link>
-          {' | '}
-          <Link to="/portal/terms" style={{ color: '#fff' }}>Terms</Link>
-          {' | '}
-          <Link to="/portal/privacy" style={{ color: '#fff' }}>Privacy Policy</Link>
-          {' | '}
-          <Link to="/portal/dashboard/home" style={{ color: '#fff' }}>Dashboard</Link>
-          {' | '}
-          <Link to="/portal/dashboard/notif" style={{ color: '#fff' }}>Notifications</Link>
-          {' | '}
-          <Link to="/portal/dashboard/schedule" style={{ color: '#fff' }}>Schedule</Link>
-          {' | '}
-          <Link to="/portal/classroom" style={{ color: '#fff' }}>Classroom</Link>
-          {' | '}
-          <Link to="/portal/syllabus/anson" style={{ color: '#fff' }}>Syllabus(A)</Link>
-        </div>
-        */}
       </div>
     </Router>
   );
