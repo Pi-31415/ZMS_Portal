2bc231da109ca4cd20d8856166db3141947fc623
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-28 15:16:02 +0630

    .

--- src/components/mycourses.js
+++ src/components/mycourses.js
@@ -1,29 +1,14 @@
-import React, { createRef, useState } from 'react';
-const Item = ({ name, price }) => (
-    <li>{name}, ${price}</li>
-)
-const App = props => {
-    let [items, setItems] = useState([
-        { id: 1, name: 'Apple', price: 0.99 },
-        { id: 2, name: 'Orange', price: 0.89 },]);
-    let nameRef = createRef(); let priceRef = createRef();
-    let add = () => {
-        let id = items.length + 1;
-        let name = nameRef.current.value; let price = priceRef.current.value;
-        setItems([...items,
-        { id, name, price }
-        ]);
-    }
+import React, { useState } from 'react';
 
-    return (<div>
-        <ul>
-            {items.map(i => (
-                <Item key={i.id}
-                    name={i.name}
-                    price={i.price} />
-            ))} 
-        </ul>
-        <input type="text" ref={nameRef} /><br /> <input type="text" ref={priceRef} /><br /> <button onClick={add}>Add</button>
-    </div>)
+export default function Example() {
+  // Declare a new state variable, which we'll call "count"  
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
-export default App;
