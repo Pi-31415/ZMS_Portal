62eafeee82f033b835f4f4cf666c2d05e5c08248
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-28 18:30:31 +0630

    Update markdownrenderer.js

--- src/components/markdownrenderer.js
+++ src/components/markdownrenderer.js
@@ -6,20 +6,37 @@ import ListItemText from '@material-ui/core/ListItemText';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import MDReactComponent from 'markdown-react-js';
+import axios from 'axios';
+var namelist = localStorage.getItem("Coursenames").split(",");
+var disclist = localStorage.getItem("Coursedisc").split(",");
 
 export default function Example(props) {
     // Declare a new state variable, which we'll call "count"  
-    //const [userid,setUserid] = useState("");
+    const [search,setSearch] = useState(props.name);
 
     useEffect(() => {    // Update the document title using the browser API  
-        
+        console.log();
+        axios.post("https://zmsedu.com/api/admin/syllabus/get", {
+            //ROLE: "Student"
+        })
+            .then(res => {
+                const syll = res.data.SYLLABUS;
+                
+                for(var i=0;i<syll.length;i++){
+                    console.log(search);
+                }
 
-    });
+            }).catch(error => {
+                alert(error);
+            });
+
+    }, []);
 
     return (
         <>
-            <h1>{props.course}</h1>
+            <h1>{namelist[parseInt(props.name) - 1]}: {props.course}</h1>
             <p>{props.name}</p>
+
         </>
     );
 }
