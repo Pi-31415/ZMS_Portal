eafb4b7827c1dfa5395b86d3acf4543098aebbde
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-28 15:16:48 +0630

    .

--- src/components/mycourses.js
+++ src/components/mycourses.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import Mycourses from '../components/mycourses';
 
 export default function Example() {
   // Declare a new state variable, which we'll call "count"  
   const [count, setCount] = useState(0);
 
 
-  
+
 
   return (
     <div>
