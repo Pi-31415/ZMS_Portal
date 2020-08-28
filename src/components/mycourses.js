d207b0e9330538027a870d9fe50671e50450586a
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-28 16:17:33 +0630

    .

--- src/components/mycourses.js
+++ src/components/mycourses.js
@@ -13,7 +13,7 @@ var enrolledcourses = [];
 export default function Example() {
     // Declare a new state variable, which we'll call "count"  
     const [userid, setUserid] = useState("");
-    const [mycourses,setMycourses] = useState([]);
+    const [mycourses, setMycourses] = useState([]);
 
     useEffect(() => {    // Update the document title using the browser API  
         setUserid(localStorage.getItem("Userid"));
@@ -21,48 +21,65 @@ export default function Example() {
         axios.post('https://zmsedu.com/api/admin/course/get', {
 
         })
-        .then(res => {
-            const courses = res.data.COURSES;
-            for(var i=0;i<courses.length;i++){
-                courselookupname.push(courses[i].NAME);
-                courselookupdescription.push(courses[i].DESCRIPTION);
-            }
-            localStorage.setItem("Coursenames", courselookupname);
-            localStorage.setItem("Coursedisc", courselookupdescription);
-        }).catch(error => {
-            console.log(error);
-        });
+            .then(res => {
+                const courses = res.data.COURSES;
+                for (var i = 0; i < courses.length; i++) {
+                    courselookupname.push(courses[i].NAME);
+                    courselookupdescription.push(courses[i].DESCRIPTION);
+                }
+                localStorage.setItem("Coursenames", courselookupname);
+                localStorage.setItem("Coursedisc", courselookupdescription);
+            }).catch(error => {
+                console.log(error);
+            });
         //then get classes
         axios.post('https://zmsedu.com/api/admin/class/get', {
 
         })
-        .then(res => {
-            const classes = res.data.CLASS;
-            for(var i=0;i<classes.length;i++){
-                //console.log(classes[i].STUDENTS);
-                if(classes[i].STUDENTS.includes(localStorage.getItem("Userid"))){
-                    enrolledcourses.push(classes[i].CLASS_ID);
+            .then(res => {
+                const classes = res.data.CLASS;
+                for (var i = 0; i < classes.length; i++) {
+                    //console.log(classes[i].STUDENTS);
+                    if (classes[i].STUDENTS.includes(localStorage.getItem("Userid"))) {
+                        enrolledcourses.push(classes[i].CLASS_ID);
+                    }
+
                 }
-                
-            }
-            console.log(enrolledcourses);
-            setMycourses(enrolledcourses);
-        }).catch(error => {
-            console.log(error);
-        });
+                console.log(enrolledcourses);
+                setMycourses(enrolledcourses);
+            }).catch(error => {
+                console.log(error);
+            });
 
     }, []);
 
+
+    let button;
+    if (mycourses[0] == undefined) {
+        button = <>
+            <ListItem button>
+                <ListItemText primary="No Classes" />
+            </ListItem>
+        </>;
+    }
+    else {
+        button = mycourses.map((course) =>
+            <>
+                <Link to="/portal/dashboard/schedule" className="custom-link-normal">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <FontAwesomeIcon icon={faCalendarAlt} size='2x' />
+                        </ListItemIcon>
+                        <ListItemText primary={course} />
+                    </ListItem>
+                </Link>
+            </>
+        );
+    }
+
     return (
         <>
-            <Link to="/portal/dashboard/schedule" className="custom-link-normal">
-                <ListItem button>
-                    <ListItemIcon>
-                        <FontAwesomeIcon icon={faCalendarAlt} size='2x' />
-                    </ListItemIcon>
-                    <ListItemText primary={userid} />
-                </ListItem>
-            </Link>
+            {button}
         </>
     );
 }
