96e6794439c09af7779bfdab595225075fdf0711
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-28 18:57:29 +0630

    .

--- src/components/markdownrenderer.js
+++ src/components/markdownrenderer.js
@@ -7,9 +7,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import MDReactComponent from 'markdown-react-js';
 import axios from 'axios';
-var namelist = localStorage.getItem("Coursenames").split(",");
-var disclist = localStorage.getItem("Coursedisc").split(",");
-
 
 export default function Example(props) {
     // Declare a new state variable, which we'll call "count"  
@@ -38,7 +35,7 @@ export default function Example(props) {
 
     return (
         <>
-            <h1>{namelist[parseInt(props.name) - 1]}: {props.course} Syllabus</h1>
+            <h1>{props.course} Syllabus</h1>
             {/*<p>{props.name}</p> */}
             <MDReactComponent text={reference} />
         </>
