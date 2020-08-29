e74bfac104fbbd6b9f25d1404b18c151eb5d10c1
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-29 17:33:49 +0630

    .

--- src/components/homeclasses.js
+++ src/components/homeclasses.js
@@ -11,14 +11,25 @@ import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 export default function Example() {
     // Declare a new state variable, which we'll call "count"  
     const [myclasses,setMyclasses] = useState([]);
+    const [classlist,setClasslist] = useState([]);
+
+    const factorclass = () =>{
+        var tempclass = [];
+        var compon = myclasses.toString().split(",");
+        for(var i=0;i<compon.length;i++){
+            tempclass.push(compon[i]);
+        }
+        setClasslist(tempclass);
+    }
 
     useEffect(() => {    // Update the document title using the browser API  
         setMyclasses(localStorage.getItem("Myclasses"));
-    });
+        factorclass();
+    },[]);
 
     return (
         <>
-            {myclasses}
+            {classlist.map((ronin)=><>{ronin}<br/></>)}
         </>
     );
 }
