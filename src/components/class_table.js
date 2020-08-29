b2a56d0323e33996b0bbbe060e37e0385ff3bab6
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-29 21:36:07 +0630

    .

---
+++ src/components/class_table.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+
+export default function Example() {
+    
+
+    useEffect(() => {    // Update the document title using the browser API  
+       
+        
+
+    });
+
+    return (
+        <>
+            Class Table
+        </>
+    );
+}
