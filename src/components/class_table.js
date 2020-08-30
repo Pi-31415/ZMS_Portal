5d9651772546d328cdb47451007fa7f3e95604f5
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-30 12:18:26 +0630

    .

--- src/components/class_table.js
+++ src/components/class_table.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
 
+
 export default function Example(props) {
     const [data, setData] = useState([]);
 
     
 
     useEffect(() => {
-        
-    },[]);
+        console.log(props.classdata);
+    },[props.classdata]);
 
     return (
         <>
-            {props.classdata}
+            
             Class Table
         </>
     );
