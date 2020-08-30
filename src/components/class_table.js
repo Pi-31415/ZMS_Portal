a6dcd6d3cf045d9eeef182d6d37082ab8896625c
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-30 15:32:35 +0630

    .

--- src/components/class_table.js
+++ src/components/class_table.js
@@ -4,22 +4,18 @@ import React, { useState, useEffect } from 'react';
 var data = [];
 
 export default function Example(props) {
-    const [mydata, setMydata] = useState([]);
 
-    const stateme = () => {
-        setMydata(data);
-        console.log(mydata);
+    let yayaya;
+    if(props.classdata != undefined){
+        yayaya = props.classdata;
+    }else{
+        yayaya = [];
     }
-
-    useEffect(() => {
-        data = props.classdata;
-        stateme();
-    }, [props.classdata]);
 
     return (
         <>
             <ul>
-                {props.classdata.map(u => <div>{u}</div>)}
+                {yayaya.map((number)=> <li>{number}</li>)}
             </ul>
         </>
     );
